07b74ecd0f184bc94e5c8ad43159cced8b022542
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-08-14 18:56:01 -0400

    filtro colores , clase abstracta

--- mueblesLab2_2024_10/codigoBase.py
+++ mueblesLab2_2024_10/codigoBase.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import List
+from typing import List, Optional
+from typing import List
+import json
+from typing import List, Type
 
 class Furniture(ABC):
   def __init__(self, model: str, width: int, height: int,
@@ -29,9 +33,6 @@ class Furniture(ABC):
 
   def __str__(self) -> str:
     return f'{self.__class__.__name__}: {self._model} ({self._width}x{self._height}x{self._depth}) - ${self.price} (x{self.shipping_factor})'
-  
-
-from typing import List
 
 class Nightstand(Furniture):
   def __init__(self, model: str, width: int, height: int,
@@ -43,9 +44,6 @@ class Nightstand(Furniture):
   @property
   def price(self) -> int:
     return 40 + (0 if self._material != "wood" else 30)
-  
-
-from typing import List, Optional
 
 class Cupboard(Furniture):
   def __init__(self, model: str, width: int, height: int,
@@ -65,11 +63,32 @@ class DinningSet(Furniture):
       super().__init__(model, width, height, depth, colors, spaces)
       self._seating: int = seating
 
-class FurnitureFilter(ABC):
-  pass
+    @property
+    def price(self) -> int:
+      return 100 + (10 * self._seating)
 
-import json
-from typing import List, Type
+#----------------------------------------------------------------------------
+
+class FurnitureFilter(ABC):
+    @abstractmethod
+    def filtroBuscado(self, furniture):
+        pass
+    def preguntarFiltro(self):
+        pass
+    def mostrarResultado(self):
+        pass
+
+class ColorFilter(FurnitureFilter):
+    def __init__(self):
+      self.pregunta = None
+    def filtroBuscado(self, furniture):
+       return self.color in furniture.colors
+    def preguntarFiltro(self):
+       self.pregunta = input("INGRESE EL FILTRO QUE DESEA APLICAR")
+    def mostrarResultado(self):
+       print(f"filtro de color: {self.color}")
+
+
 
 class Store:
   def __init__(self) -> None:
@@ -77,16 +96,14 @@ class Store:
 
   def load_furnitures(self, filename: str) -> None:
     with open(filename, 'r') as f:
-      data = json.load(f)
-      print(data)
+        data = json.load(f)
 
-      for fur in data:
-        class_name = fur.pop("type")
-        if class_name in globals():
-          self._furnitures.append(globals()[class_name](**fur))
+        for fur in data:
+            class_name = fur.pop("type")
+            if class_name in globals():
+                self._furnitures.append(globals()[class_name](**fur))
 
   def load_filters(self) -> None:
-    # Ud. deben implementarlo
     ...
 
   def start(self) -> None:
