1b4533a478d9a5641fe2fa1b6a4237421f554d74
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-08-17 16:07:50 -0400

    meotodos creados para la clase ataque

--- tarea1PDP/classRobot/ataque.py
+++ tarea1PDP/classRobot/ataque.py
@@ -1,15 +1,60 @@
 class Ataque:
     def __init__(self, nombre, tipo, objetivo, daño, precision, recarga, turnos_restantes):
+        #indica el nombre del ataque
         self.nombre = nombre
-        self.tipo = tipo
-        self.objetivo = objetivo
-        self.daño = daño
-        self.precision = precision
+        #long o sword o hand
+        self.tipo = tipo 
+        #team o robot
+        self.objetivo = objetivo  
+        #int cuanto daño hace el ataque
+        self.daño = daño 
+        #int porcentaje que indica la cantidad de veces que el ataque acierta su objetivo
+        self.precision = precision 
+        #int cuantos turnos debe esperar antes de poder volver a usar este ataque
         self.recarga = recarga
+        #int que muestra cuantos turnos le faltan para volver a usar el ataque
         self.turnos_restantes = turnos_restantes
+
+    def get_nombre_ataque(self):
+        return self.nombre
+
+    def get_tipo_ataque(self):
+        return self.tipo
+
+    def get_objetivo_ataque(self):
+        return self.objetivo
+    
+    def get_daño(self):
+        return self.daño
+    
+    def get_precision(self):
+        return self.precision
+    
+    def get_recarga(self):
+        return self.recarga
+    
+    def get_turnos_restantes(self):
+        return self.turnos_restantes
     
     def puede_usarse(self):
-        pass
-    
+        if self.turnos_restantes == 0:
+            return True
+        else:
+            return False
+        
+    #metodo para preguntar al ususario si desea utilizar la habilidad
     def usar(self):
-        pass
+        if self.puede_usarse() == True:
+            self.turnos_restantes = self.turnos_restantes + self.recarga
+            return self.daño
+        else:
+            return f"EL ATAQUE {self.nombre.upper()} NO PUEDE USARSE, SE ESTA RECARGANDO. TURNOS RESTANTES: {self.turnos_restantes}"
+
+
+ataque1= Ataque("rasengan", "hand", "robot", 10, 97, 1, 0)
+print(ataque1.puede_usarse())
+print(ataque1.get_turnos_restantes())
+ataque1.usar()
+print(ataque1.get_turnos_restantes())
+print(ataque1.puede_usarse())
+print(ataque1.usar())
