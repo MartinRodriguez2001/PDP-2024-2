b91540d53f6eb60ed734274a57e6ae6ce3d5852b
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-08-17 16:08:19 -0400

    metodos creados para la clase habilidad

--- tarea1PDP/classRobot/habilidad.py
+++ tarea1PDP/classRobot/habilidad.py
@@ -1,17 +1,23 @@
 class Habilidad:
-    def __init__(self, nombre, activador, duracion, objetivo, efecto, activo):
+    def __init__(self, nombre, activador, duracion, objetivo, efecto):
         self.nombre = nombre
         self.activador = activador
         self.duracion = duracion
         self.objetivo = objetivo
         self.efecto = efecto
-        self.activo = activo
+        self.activo = bool
+    
+    def get_nombre_habilidad(self):
+        return self.nombre
+    
+    def isActive(self):
+        return self.activo
     
     def activar(self):
-        pass
+        return self.activo == True
 
     def desactivar(self):
-        pass
+        return self.activo == False
 
     def aplicar_efecto(self, robot):
         pass
