b6ec22e2b04ee3a75598053a18ce94c8565f7c1d
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-08-25 18:35:46 -0400

    estructura clase reporte

--- tarea1PDP/reporte.py
+++ tarea1PDP/reporte.py
@@ -8,3 +8,43 @@ class Reporte:
 
     def guardar_reporte(self, formato):
         pass
+
+    def eleccion_reporte(self):
+        while True:
+            print("REPORTES DE PELEAS")
+            print("1. REPORTE DE VICTORIAS/DERROTAS")
+            print("2. GRAFICO DE BARRAS ATAQUES")
+            print("3. GRAFICO DE BARRAS HABILIDAD")
+            print("4. TABLA DE BATALLAS")
+            print("5. VOLVER")
+
+            try:
+                opcion = int(input("SELECCIONA LOS REPORTES QUE DESEAS VISUALIZAR"))
+                print("\n")
+                
+                if opcion == 1:
+                    pass
+                elif opcion == 2:
+                    pass
+                elif opcion == 3:
+                    pass
+                elif opcion == 4:
+                    pass
+                elif opcion == 5:
+                    break
+
+            except ValueError:
+                print("OPCION INVALIDA. INGRESA NUEVAMENTE")
+                continue
+
+    def reporte_victorias_derrotas(self):
+        pass
+
+    def reporte_grafico_ataques(self):
+        pass
+
+    def reporte_grafico_habilidad(self):
+        pass
+
+    def tablas_batallas(self):
+        pass
