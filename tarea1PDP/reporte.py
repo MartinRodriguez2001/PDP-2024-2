3c26d588522b43475d818e609324f237ee9165dc
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-08-26 02:37:42 -0400

    escritura de archivo activa

--- tarea1PDP/reporte.py
+++ tarea1PDP/reporte.py
@@ -1,6 +1,7 @@
+import csv
 class Reporte:
-    def __init__(self, tipo):
-        self.tipo = tipo
+    def __init__(self, robots):
+        self.robots = robots
         self.datos = {}
     
     def generar_reporte(self):
@@ -13,9 +14,9 @@ class Reporte:
         while True:
             print("REPORTES DE PELEAS")
             print("1. REPORTE DE VICTORIAS/DERROTAS")
-            print("2. GRAFICO DE BARRAS ATAQUES")
-            print("3. GRAFICO DE BARRAS HABILIDAD")
-            print("4. TABLA DE BATALLAS")
+            print("2. GRAFICO DE BARRAS ATAQUES (PROXIMAMENTE)")
+            print("3. GRAFICO DE BARRAS HABILIDAD (PROXIMAMENTE)")
+            print("4. TABLA DE BATALLAS (PROXIMAMENTE)")
             print("5. VOLVER")
 
             try:
@@ -23,7 +24,9 @@ class Reporte:
                 print("\n")
                 
                 if opcion == 1:
-                    pass
+                    datos = self.ordenar_lista()
+                    self.reporte_victorias_derrotas(datos)
+
                 elif opcion == 2:
                     pass
                 elif opcion == 3:
@@ -37,8 +40,20 @@ class Reporte:
                 print("OPCION INVALIDA. INGRESA NUEVAMENTE")
                 continue
 
-    def reporte_victorias_derrotas(self):
-        pass
+    def reporte_victorias_derrotas(self, datos):
+        nombre_archivo = "tabla_victorias_derrotas.csv"
+        encabezados = ["NOMBRE", "VICTORIAS"]
+
+        with open(nombre_archivo, mode ="w", newline="") as archivo:
+            writer = csv.writer(archivo)
+            writer.writerow(encabezados)
+            writer.writerow("\n")
+            
+            for fila in datos:
+                writer.writerow(fila)
+                archivo.write("\n")
+
+        print(f"EL ARCHIVO {nombre_archivo} SE HA CARGADO CORRECTAMENTE")
 
     def reporte_grafico_ataques(self):
         pass
@@ -48,3 +63,8 @@ class Reporte:
 
     def tablas_batallas(self):
         pass
+
+    def ordenar_lista(self):
+        self.robots.sort(key= lambda x: x.victorias, reverse = True)
+        lista_ordenados = [[robot.nombre, robot.victorias] for robot in self.robots]
+        return lista_ordenados
